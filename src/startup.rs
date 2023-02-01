8f93fa8d39d98b41b976cb853f3d375f0d2f5785
Peter Bendel <peter@familie-bendel.de>
2023-02-01 19:39:07 +0100

    login form with flash message validated by HMAC (stored in cookie)

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,20 @@
+use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::ExposeSecret;
+use secrecy::Secret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
-
-use crate::configuration::{DatabaseSettings, Settings};
-use sqlx::postgres::PgPoolOptions;
 
 pub struct Application {
     port: u16,
@@ -46,6 +49,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -68,12 +72,18 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let base_url = Data::new(ApplicationBaseUrl(base_url));
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
+
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
@@ -85,6 +95,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
@@ -97,3 +108,6 @@ pub async fn get_connection_pool(configuration: &DatabaseSettings) -> Result<PgP
         .connect_with(configuration.with_db())
         .await
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
